9c940ea587459bf28a94a6e84d32f441fcd6e221
Honore <your@email.com>
2026-03-25 22:09:11 +0200

    add sitemap generation

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,14 +1,8 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.NEXT_PUBLIC_SITE_URL || 'https://afriearners.vercel.app',
+  siteUrl: 'https://africa-earners.vercel.app',
   generateRobotsTxt: true,
   changefreq: 'daily',
   priority: 0.7,
   sitemapSize: 5000,
-  robotsTxtOptions: {
-    policies: [
-      { userAgent: '*', allow: '/' },
-      { userAgent: 'Googlebot', allow: '/' },
-    ],
-  },
 }
